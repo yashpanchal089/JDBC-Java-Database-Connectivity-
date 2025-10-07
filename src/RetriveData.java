d62011d71f85e246dc6643858ce9a66b70cb830b
Yash Panchal <panchalyash089@gmail.com>
2025-10-07 17:37:13 +0530

    Retrive Table data from mysql

---
+++ src/RetriveData.java
@@ -0,0 +1,42 @@
+import java.sql.*;
+
+public class FirstProgram {
+    public static void main(String[] args) throws ClassNotFoundException {
+
+        String url = "jdbc:mysql://localhost:3306/javajdbc";
+        String username = "root";
+        String password = "Shayona@2025";
+        String query = "SELECT * FROM employees";
+
+
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Driver loaded Successfully");
+        } catch (ClassNotFoundException e){
+            System.out.println(e.getMessage());
+        }
+
+        try{
+            Connection conn = DriverManager.getConnection(url, username, password);
+            System.out.println("Connection established Successfully");
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+
+            while (rs.next()){
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                String job_title = rs.getString("job_title");
+                double salary = rs.getDouble("salary");
+                System.out.println("------------------------------");
+                System.out.println("Id : " + id);
+                System.out.println("Name : " + name);
+                System.out.println("Job Title : " + job_title);
+                System.out.println("Salary : " + salary);
+            }
+            conn.close();
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
