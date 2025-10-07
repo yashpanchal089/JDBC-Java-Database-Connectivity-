ce9c713d52eca859597fda85ed7da0116edd98d5
Yash Panchal <44561539+yashpanchal089@users.noreply.github.com>
2025-10-07 17:17:54 +0530

    Change database connection details in Main.java
    Updated database connection parameters to use placeholders.

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3306/javajdbc";
-        String user = "root";
-        String password = "Shayona@2025";
+        String url = "jdbc:mysql://localhost:3306/db_name";
+        String user = "your_username";
+        String password = "your_password";
 
         try (Connection conn = DriverManager.getConnection(url, user, password)){
             System.out.println("Connected to the database successfully!");
