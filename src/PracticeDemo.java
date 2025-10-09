d269701d1fb6a144ad36d864f218ed89c163cd25
Yash Panchal <panchalyash089@gmail.com>
2025-10-09 15:11:14 +0530

    Practicing for the retrieving and inserting the data

---
+++ src/PracticeDemo.java
@@ -0,0 +1,41 @@
+import java.sql.*;
+import java.util.stream.StreamSupport;
+
+public class PracticeDemo {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+        String url = "jdbc:mysql://localhost:3306/javajdbc";
+        String username = "root";
+        String password = "Shayona@2025";
+//        String query = "INSERT into employees(id, name, job_title, salary) values (9, 'Alert', 'CMO', 636366);";
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        System.out.println("Driver Loaded Successfully");
+
+        Connection conn = DriverManager.getConnection(url, username, password);
+        System.out.println("Connection Established Successfully");
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT id, name, job_title, salary FROM employees;");
+//        int rowsAffected = stmt.executeUpdate(query);
+
+        while(rs.next()){
+            int id = rs.getInt("id" );
+            String name = rs.getString("name");
+            System.out.println("ID: " + id );
+            System.out.println("Name: " + name);
+        }
+//
+//        if(rowsAffected>0){
+//            System.out.println("Insertion Successful, rows affected: " + rowsAffected);
+//        } else {
+//            System.out.println("Insertion Failed, no rows affected.");
+//        }
+
+        conn.close();
+        stmt.close();
+        System.out.println("Connection Closed Successfully");
+
+
+
+    }
+}
